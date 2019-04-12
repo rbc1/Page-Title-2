b11cf1e3e43d3744cc68d7ea54202fa733452df3
rbc1 <44611342+rbc1@users.noreply.github.com>
2019-04-12 09:17:31 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,25 @@
+$("button").click(function(){
+    
+});
+
+
+
+ var currentyear;
+ var typeofshoes ;
+ var shoespermonth;
+ 
+ $(".btn").click(function(){
+     currentyear=$("#current-year").val();
+       var year=2019;
+     $(".shoes-per-month").text();
+ });
+ 
+ 
+ 
+$(".btn").click(function(){
+     typeofshoes=$("#type-of-shoes").val();
+       var year=2019;
+     $(".shoes").text();
+ });
+ 
+  
